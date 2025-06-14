2bb1da0e7554cd30b8275302b6db9193f4e7d444
Mihail Mihaylov <misho.mihaylov@gmail.com>
2025-06-14 22:52:05 +0300

    Working on JWT implementation

---
+++ backend/src/main/java/invoiceCreator/backend/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package invoiceCreator.backend.config;
+
+
+import invoiceCreator.backend.jwt.JwtAuthFilter;
+import invoiceCreator.backend.user.service.Impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+public class SecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final UserServiceImpl userService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationProvider authProvider;
+
+    @Autowired
+    public SecurityConfig(JwtAuthFilter jwtAuthFilter, UserServiceImpl userService, PasswordEncoder passwordEncoder, AuthenticationProvider authProvider) {
+        this.jwtAuthFilter = jwtAuthFilter;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+        this.authProvider = authProvider;
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth.requestMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated())
+                .authenticationProvider(authProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userService);
+        provider.setPasswordEncoder(passwordEncoder);
+        return provider;
+    }
+}
